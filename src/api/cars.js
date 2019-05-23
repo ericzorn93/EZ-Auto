9bbabdb544abb3e9c7f46628228625f2828fefc5
Eric Zorn <ericzorndesigns@gmail.com>
2019-05-23 08:07:45 -0400

    Added extra description to mb

--- src/api/cars.js
+++ src/api/cars.js
@@ -4,6 +4,10 @@ const router = express.Router();
 const fetchMercedes = require("../utils/functions/fetchMercedes");
 const fetchBmw = require("../utils/functions/fetchBmw");
 
+/**
+ * @description
+ * Fetches all interested BMW models and returns the array of the desired models
+ */
 router.get("/all-cars", async (req, res) => {
   const mercedes = await fetchMercedes();
   const bmw = await fetchBmw();
